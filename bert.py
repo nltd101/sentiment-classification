e5466d2802146f8e7eb6b986c766c9cfcad244a2
Dat Nguyen <nltd101@gmail.com>
2021-07-09 11:51:05 +0700

    first commit

---
+++ bert.py
@@ -0,0 +1,99 @@
+from tensorflow.keras import models
+from django.conf import settings
+import os
+from tensorflow.python.framework.ops import disable_eager_execution
+disable_eager_execution()
+os.environ['TF_KERAS'] = '1'
+from keras_bert.layers import Extract
+import numpy as np
+from keras_bert import Tokenizer
+import pickle
+import codecs
+from tensorflow import keras
+from keras_bert import load_trained_model_from_checkpoint
+
+
+SEQ_LEN = 256
+LR = 2e-5
+
+token_dict = {}
+
+
+class bert():
+
+    def __init__(self): 
+        self.create_model()
+
+    def load_data(self, data, sentiments):      
+        token_dict = {}
+        indices = []
+        for text in data:
+            ids, segments = self.tokenizer.encode(text, max_len=SEQ_LEN)
+            indices.append(ids)
+
+        return [indices, np.zeros_like(indices)], np.array(sentiments)
+
+    def classify_sentiment(self, text:str)->float:
+        """classify sentiment of a sentence
+
+        Args:
+            text (str): A sentence predicted
+
+        Returns:
+            probability[float]: positive probability of the sentence
+        """        
+        print("list", text)
+        list_text=[text]
+        sample, _ = self.load_data(list_text, [])
+        return self.model.predict(sample)
+        # result = []
+        
+        # for i in range(len(list_text)):
+        #     if np.round(probability[i]) == 1:
+        #         result.append("Tích cực")
+        #     else:
+        #         result.append("Tiêu cực")
+        # return result
+
+    def create_model(self):
+
+        vocab_path = os.path.join("trained_model", 'vocab.txt')
+        with codecs.open(vocab_path, 'rb', 'utf-8') as reader:
+            print(vocab_path)
+            for line in reader:
+                token = line.strip()
+                # the first word is the most negative
+                token_dict[token] = len(token_dict)
+        self.tokenizer = Tokenizer(token_dict, cased=True)
+
+        self.model = load_trained_model_from_checkpoint(
+            os.path.join(
+                         "trained_model", 'bert_config.json'),
+            os.path.join(
+                         "trained_model", 'bert_model.ckpt'),
+            trainable=True,
+            seq_len=SEQ_LEN,
+            output_layer_num=4
+        )
+
+        inputs = self.model.inputs[:2]
+        newout = Extract(index=0)(self.model.output)
+        newout = keras.layers.Dense(768, activation='relu')(newout)
+        outputs = keras.layers.Dense(units=1, activation='sigmoid')(newout)
+
+        self.model = models.Model(inputs, outputs)
+
+
+       
+        self.model.load_weights(os.path.join(
+            "weights", 'lastweight'))
+
+
+
+
+
+
+
+  
+
+
---
+++ main.py
@@ -0,0 +1,57 @@
+
+from urllib.parse import urlparse, parse_qs
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+from bert import bert
+hostName = "localhost"
+serverPort = 8080
+A = bert()
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if (self.path[:5] == "/api/"):
+            self.do_reponse_GET_API()
+        else:
+            try:
+                with open('index.html', 'rb') as f:
+                    self.add_parameter('text/html')
+                    self.wfile.write(f.read())
+
+                return
+            except IOError:
+                self.send_error(404, 'File Not Found: % s' % self.path)
+
+    def do_reponse_GET_API(self):
+        print(self.path)
+        parsed_url = urlparse(self.path)
+        query = parse_qs(parsed_url.query)
+        print(query, type(query))
+        self.add_parameter("application/json")
+        if ("sentence" in query):
+            # self.wfile.write(bytes("<p>Request: %s</p>" %
+            #                  query["sentence"], "utf-8"))
+            print(query["sentence"][0])
+            self.wfile.write(
+                bytes('{"result":"'+str(A.classify_sentiment(query["sentence"][0])[0][0])+'"}', "utf-8"))
+
+        else:
+            self.wfile.write(
+                bytes('{err:"sentence is required"}', "utf-8"))
+
+    def add_parameter(self, arg0):
+        self.send_response(200)
+        self.send_header("Content-type", arg0)
+        self.end_headers()
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
